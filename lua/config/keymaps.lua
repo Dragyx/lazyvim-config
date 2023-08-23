ba36515f921d53efe0ff1987b2aa5a78c7767137
Dragyx <66752602+Dragyx@users.noreply.github.com>
2023-08-23 18:10:57 +0200

    Change gt and gT keybinds to buffer commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Change default nvim tabnext and tabprevious to buffers, because
+-- neotree creates new buffers and not tabs
+vim.keymap.set("n", "gt", "<cmd>bnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "gT", "<cmd>bprevious<cr>", { desc = "Previous Tab" })
