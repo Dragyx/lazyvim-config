bc9978c054d0a722ded43d6967c8af0409690306
Dragyx <66752602+Dragyx@users.noreply.github.com>
2023-09-05 18:44:07 +0200

    Change default shell to nu

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Use nushell if available
+-- (vim.fn.executable returns 1 if the binary exists)
+vim.opt.shell = vim.fn.executable "nu" and "nu" or "bash"
